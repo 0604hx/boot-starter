02ae4e9270c05876bff1eab083f046bb19232cad
0604hx <zxingming@qq.com>
2023-03-10 18:09:05 +0800

    Form.java 增加 NUMBER 属性

--- src/main/java/org/nerve/boot/enums/Form.java
+++ src/main/java/org/nerve/boot/enums/Form.java
@@ -5,5 +5,5 @@ package org.nerve.boot.enums;
  * Created by fdt on 17/4/2.
  */
 public enum Form {
-    TEXT,TEXTAREA,SELECT,RADIO,HIDDEN,PASSWORD
+    TEXT,TEXTAREA,SELECT,RADIO,HIDDEN,PASSWORD,NUMBER
 }
